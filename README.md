26f35b623bf743641672a42b74ab832192280bc5
Dwayne R. Crooks <me@dwaynecrooks.com>
2015-12-03 11:14:47 -0400

    Build the home page

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Dwayne's Personal Website
+# About
+
+My personal website.
+
+# Notes
+
+**Building and viewing the site locally**
+
+```sh
+$ nvm use
+$ npm install
+$ gulp
+```
+
+# Resources
+
+- [HTML5 Boilerplate](https://github.com/h5bp/html5-boilerplate)
+- [Normalize.css](http://necolas.github.io/normalize.css/)
+- [Node Version Manager](https://github.com/creationix/nvm)
+- [Node](https://nodejs.org/en/)
+- [Gulp](http://gulpjs.com/)
+- [Browsersync](http://www.browsersync.io/)
+- [Jade - Template Engine](http://jade-lang.com/)
+- [Sass](http://sass-lang.com/)
+ - [Breakpoint](http://breakpoint-sass.com/)
+- [PostCSS](https://github.com/postcss/postcss)
+ - [Autoprefixer](https://github.com/postcss/autoprefixer)
+ - [Font Magician](https://github.com/jonathantneal/postcss-font-magician)
--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,45 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
 
 gulp.task('jade', function() {
   return gulp.src('./src/templates/views/**/*.jade')
     .pipe($.jade())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['jade']);
+gulp.task('stylesheets', function() {
+  return gulp.src('./src/stylesheets/index.scss')
+    .pipe(
+      $.sass({
+        includePaths: [
+          './node_modules/normalize.css',
+          './node_modules/breakpoint-sass/stylesheets'
+        ]
+      })
+      .on('error', $.sass.logError)
+    )
+    .pipe($.postcss([
+      require('autoprefixer'),
+      require('postcss-font-magician')()
+    ]))
+    .pipe(gulp.dest('./dist/stylesheets'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['jade', 'stylesheets'], function() {
+  browserSync.init({
+    server: './dist',
+    port: 8000,
+    ui: {
+      port: 8001
+    }
+  });
+
+  gulp.watch('./src/stylesheets/**/*.scss', ['stylesheets']);
+  gulp.watch('./src/templates/**/*.jade', ['jade']);
+});
+
+gulp.task('default', ['serve']);
